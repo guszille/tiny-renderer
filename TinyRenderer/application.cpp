5aeeba92b927b6b2d7de2cafa7f4dfc921a937f3
Gustavo Zille <guzillaine@gmail.com>
2023-10-01 23:09:46 -0300

    Implementing z-buffer to the renderer.

--- TinyRenderer/application.cpp
+++ TinyRenderer/application.cpp
@@ -56,43 +56,61 @@ void draw_line(int x0, int y0, int x1, int y1, Image& image, const Color& color)
     }
 }
 
-Vec3f get_barycentric_coords(Vec2i* points, Vec2i P)
+Vec3f get_barycentric_coords(Vec3f A, Vec3f B, Vec3f C, Vec3f P)
 {
-    Vec3f u = Vec3f(points[2][0] - points[0][0], points[1][0] - points[0][0], points[0][0] - P[0]) ^ Vec3f(points[2][1] - points[0][1], points[1][1] - points[0][1], points[0][1] - P[1]);
+    Vec3f s[2] = { {C[0] - A[0], B[0] - A[0], A[0] - P[0]}, {C[1] - A[1], B[1] - A[1], A[1] - P[1]} };
+    Vec3f u = s[0]^s[1];
 
-    if (std::abs(u.z) < 1) return Vec3f(-1.0f, -1.0f, -1.0f); // If the triangle is degenerate, return something with negative coordinates.
-    
-    return Vec3f(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    if (std::abs(u[2]) > 1e-2)
+    {
+        return Vec3f(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    }
+
+    return Vec3f(-1.0f, -1.0f, -1.0f);
 }
 
-void draw_triangle(Vec2i* points, Image& image, const Color& color)
+void draw_triangle(Vec3f* points, float* zbuffer, Image& image, const Color& color)
 {
-    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
-    Vec2i bboxmax(0, 0);
-    Vec2i   clamp(image.get_width() - 1, image.get_height() - 1);
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f   clamp(image.get_width() - 1, image.get_height() - 1);
 
     for (int i = 0; i < 3; i++)
     {
-        bboxmin.x = std::max(0, std::min(bboxmin.x, points[i].x));
-        bboxmin.y = std::max(0, std::min(bboxmin.y, points[i].y));
-
-        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, points[i].x));
-        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, points[i].y));
+        for (int j = 0; j < 2; j++)
+        {
+            bboxmin[j] = std::max(    0.0f, std::min(bboxmin[j], points[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], points[i][j]));
+        }
     }
 
-    Vec2i P;
+    Vec3f P;
 
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
     {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
         {
-            Vec3f bc_screen = get_barycentric_coords(points, P);
+            Vec3f bccoords = get_barycentric_coords(points[0], points[1], points[2], P);
 
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+            if (bccoords.x < 0 || bccoords.y < 0 || bccoords.z < 0) continue;
 
-            image.set(P.x, P.y, color);
+            P.z = 0;
+
+            for (int i = 0; i < 3; i++) P.z += points[i][2] * bccoords[i];
+
+            if (zbuffer[int(P.x + P.y * IMAGE_WIDTH)] < P.z)
+            {
+                zbuffer[int(P.x + P.y * IMAGE_WIDTH)] = P.z;
+
+                image.set(P.x, P.y, color);
+            }
         }
     }
+}
+
+Vec3f world_to_screen(Vec3f v)
+{
+    return Vec3f(int((v.x + 1.0f) * IMAGE_WIDTH / 2.0f + 0.5f), int((v.y + 1.0f) * IMAGE_HEIGHT / 2.0f + 0.5f), v.z);
 }
 
 int main(int argc, char** argv)
@@ -102,31 +120,32 @@ int main(int argc, char** argv)
 
     Vec3f light_direction(0.0f, 0.0f, -1.0f);
 
+    float* zbuffer = new float[IMAGE_WIDTH * IMAGE_HEIGHT];
+    for (int i = IMAGE_WIDTH * IMAGE_HEIGHT; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
     for (int i = 0; i < model.get_nof_faces(); i++)
     {
         std::vector<int> face = model.get_face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
+        Vec3f wcoords[3]; // World coordinates.
+        Vec3f scoords[3]; // Screen coordinates.
 
         for (int j = 0; j < 3; j++)
         {
-            Vec3f v = model.get_vertex(face[j]);
-
-            screen_coords[j] = Vec2i((int)((v.x + 1.0f) * IMAGE_WIDTH / 2.0f), (int)((v.y + 1.0f) * IMAGE_HEIGHT / 2.0f));
-            world_coords[j] = v;
+            wcoords[j] = model.get_vertex(face[j]);
+            scoords[j] = world_to_screen(wcoords[j]);
         }
 
-        Vec3f face_normal = ((world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0])).normalize();
+        Vec3f face_normal = ((wcoords[2] - wcoords[0]) ^ (wcoords[1] - wcoords[0])).normalize();
         float intensity = face_normal * light_direction;
 
         if (intensity > 0.0f)
         {
-            draw_triangle(screen_coords, image, Color(intensity * 255, intensity * 255, intensity * 255, 255));
+            draw_triangle(scoords, zbuffer, image, Color(intensity * 255, intensity * 255, intensity * 255, 255));
         }
     }
 
     image.flip_vertically();
-    image.write_on_disk("outputs/framebuffer_2.jpg", Image::FileFormat::JPG);
+    image.write_on_disk("outputs/framebuffer_3.jpg", Image::FileFormat::JPG);
 
     return 0;
 }
